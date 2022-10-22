93422b072ec362222e03380d887dd301807cf086
akinyosoyeisaac <akinyosoyeisaac@gmail.com>
2022-10-22 22:13:16 +0100

    streamlit app added

---
+++ streamlit_app/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from inference import prediction
+
+@st.cache
+def convert_csv(file):
+    return file.to_csv()
+
+def main():
+    st.set_page_config(page_title="KOWOPE-MARK CCDection")
+    st.title("KOWOPE-MART CREDIT CARD CUSTOMER DEFAULT DETECTION")
+    
+    st.header("Upload File For Inference")
+    uploadfile = st.file_uploader("", type="csv", accept_multiple_files=False)
+    if uploadfile is not None:
+        with st.spinner("prediction is been generated..."):
+            predict = prediction(uploadfile)
+            st.success("Prediction Generated")
+        if st.button("Check Inference"):
+            st.dataframe(predict)
+        download = convert_csv(predict)
+        st.download_button("Download Predictions", download, "prediction.csv", mime="test/csv")
+            
+if __name__ == "__main__":
+    main()
---
+++ streamlit_app/inference.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+import pickle as pk
+
+
+def prediction(data):
+    test = pd.read_csv(data)
+    mapping = {"charge": 1, "lending":0}
+    test["form_field47"] = test['form_field47'].map(mapping)
+    # dropping column with high cardinality
+    applicant_id = test["Applicant_ID"]
+    test.drop(columns=["Applicant_ID"], inplace = True)
+    # dropping columns that are highly correlated
+    columns = ["form_field1", "form_field2", "form_field3", "form_field4", "form_field7", "form_field8", "form_field9", "form_field16", "form_field19", "form_field24", "form_field29", "form_field33", "form_field34", "form_field36", "form_field38", "form_field43", "form_field44", "form_field47", "form_field48", "form_field50"]
+    test = test[columns]
+    impute_missing = SimpleImputer()
+    test = impute_missing.fit_transform(test)
+    test = pd.DataFrame(test, columns=columns)
+    with open("model\model_rf.pk", "rb") as file:
+        model = pk.load(file)
+    predict = model.predict(test)
+    predict = pd.DataFrame({"Applicant_ID": applicant_id, "Default_Status": predict})
+    predict["Default_Status"] = predict["Default_Status"].map({0:"        No", 1:"        Yes"})
+    # predict.set_index("Applicant_ID", inplace=True)
+    return predict
