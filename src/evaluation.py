515e211363dee9668403c78de37aa37a649b6c13
akinyosoyeisaac <akinyosoyeisaac@gmail.com>
2022-10-25 14:46:43 +0100

    building model pipeline updated

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,23 @@
- 
+import matplotlib.pyplot as plt 
+from sklearn.metrics import classification_report, RocCurveDisplay, ConfusionMatrixDisplay
+import json
+from training import train
+
+def reporting():
+    y_test, y_pred = train()
+    
+    classification_report = classification_report(y_test, y_pred)
+    
+    classification_report_dict = {"classification_report": classification_report}
+    with open("classification_report.json", "w"):
+        json.dump({"classification"})
+        
+    fig, ax = plt.subplots(figsize=(5,5))
+    ConfusionMatrixDisplay.from_predictions(y_true=y_test, y_pred=y_pred, normalize="true", labels=[0,1], display_labels=["no", "yes"], ax=ax)
+    ax.set_title("CONFUSION MATRIX RF");
+    plt.savefig("report/confusion_matrix_rf.jpg")
+
+    fig, ax = plt.subplots(figsize=(5,5))
+    RocCurveDisplay.from_predictions(y_true=y_test, y_pred=y_pred, name="ROC PIPE XGB", ax=ax)
+    ax.set_title("ROC CURVE RF");
+    plt.savefig("report/roc_curve_rf.jpg")
--- src/training.py
+++ src/training.py
@@ -2,7 +2,39 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import make_pipeline
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import classification_report, RocCurveDisplay, ConfusionMatrixDisplay
 import pickle as pk
+
+def train():
+    train_copy = pd.read_csv("data/train_processed.csv")
+    
+    X, y = train_copy.drop(columns="default_status"), train_copy["default_status"]
+    
+    over_sampling = SMOTE(random_state=234, k_neighbors=13)
+    X_oversampling, y_oversampling = over_sampling.fit_resample(X, y)
+    train_copy_res = pd.concat([X_oversampling, y_oversampling], axis=1)
+    
+    X, y = train_copy_res.drop(columns="default_status"), train_copy_res["default_status"]
+    sss = StratifiedShuffleSplit(test_size=0.2, random_state=234)
+    for train_index, test_index in sss.split(X, y):
+        X_train, X_test = X.loc[train_index], X.loc[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        
+    impute_missing = SimpleImputer()
+    scaler = StandardScaler()
+    
+    rf = RandomForestClassifier(random_state= 1)
+    pipe_rf = make_pipeline(scaler, rf)
+    pipe_rf.fit(X_train, y_train)
+    y_pred_rf = pipe_rf.predict(X_test)
+    
+    with open("model/model_rf.pk", "wb") as file:
+        pk.dump(pipe_rf, file)
+        
+    return y_test, y_pred_rf
+    
+    
+    
